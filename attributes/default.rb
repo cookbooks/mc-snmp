4eef8e4e8cfcd831c644e7bf4a83d6e09cf59a2d
Stephen Lauck <stpehen.lauck@gmail.com>
2012-08-10 11:58:58 -0700

    added snmpd.conf and variables from attributes

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default[:snmp][:syslocation]              = "Joyent"
+default[:snmp][:syscontact]               = "sysops@modcloth.com"
+default[:snmp][:rocommunity]              = "fR@55!NG"
+default[:snmp][:rwcommunity]              = "private"
+default[:snmp][:slash_disk_threshold]     = "80" #Percent
+default[:snmp][:slash_tmp_disk_threshold] = "80" #Percent
--- recipes/default.rb
+++ recipes/default.rb
@@ -19,3 +19,17 @@ end
 service "snmp" do
   action [ :enable, :start ]
 end
+
+template "/opt/local/etc/snmpd.conf" do
+  source "snmpd.conf.erb"
+  mode "0644"
+  variables(
+      :syslocation => node[:snmp][:syslocation],
+      :syscontact => node[:snmp][:syscontact],
+      :rocommunity => node[:snmp][:rocommunity],
+      :rwcommunity => node[:snmp][:rwcommunity],
+      :slash_disk_threshhold => node[:snmp][:slash_disk_threshold],
+      :slash_tmp_disk_threshold => node[:snmp][:slash_tmp_disk_threshold]      
+    )
+  notifies :restart, "service[snmp]"
+end
