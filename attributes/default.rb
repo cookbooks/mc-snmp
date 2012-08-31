64c1f0c7deae4c28f75bb3404e22c3cc2e83fe10
Stephen Lauck <stpehen.lauck@gmail.com>
2012-08-31 16:24:18 -0700

    added custom paths/service names for various os's

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,11 @@
+case node['platform']
+when "smartos"
+  default[:snmp][:dir]                      = '/opt/local/etc'
+  default[:snmp][:service_name]             = 'snmp'
+else
+  default[:snmp][:dir]                      = '/etc/snmp'
+  default[:snmp][:service_name]             = 'snmpd'
+end
 default[:snmp][:syslocation]              = "Joyent"
 default[:snmp][:syscontact]               = "sysops@modcloth.com"
 default[:snmp][:rocommunity]              = "fR@55!NG"
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,17 +10,19 @@
 case node['platform']
 when 'smartos'
   pkg = 'net-snmp'
+else
+  pkg = 'snmpd'
 end
 
 package pkg do
   action :install
 end
 
-service "snmp" do
+service node[:snmp][:service_name] do
   action [ :enable, :start ]
 end
 
-template "/opt/local/etc/snmpd.conf" do
+template "#{node[:snmp][:dir]}/snmpd.conf" do
   source "snmpd.conf.erb"
   mode "0644"
   variables(
@@ -31,5 +33,5 @@ template "/opt/local/etc/snmpd.conf" do
       :slash_disk_threshhold => node[:snmp][:slash_disk_threshold],
       :slash_tmp_disk_threshold => node[:snmp][:slash_tmp_disk_threshold]      
     )
-  notifies :restart, "service[snmp]"
+  notifies :restart, "service[#{node[:snmp][:service_name]}]"
 end
